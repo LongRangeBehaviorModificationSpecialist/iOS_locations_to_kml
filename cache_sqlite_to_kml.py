8fb7c0ba5091b114500c2aa8ef285bbbf0a01bc6
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-08-11 15:55:31 -0400

    Update cache_sqlite_to_kml.py

--- cache_sqlite_to_kml.py
+++ cache_sqlite_to_kml.py
@@ -3,7 +3,8 @@
 from rich.console import Console
 import time
 
-from functions.functions import HelperFunctions as hf
+from make_kml_cli import ConversionArgs
+from functions.functions import Utils
 from vars.cache_sqlite import (
     cache_sqlite_query,
     cache_sqlite_kml_file_header,
@@ -14,18 +15,9 @@ from vars.cache_sqlite import (
 c = Console()
 
 
-def write_cache_sqlite_to_kml(
-        python_file: str,
-        source: str,
-        dest: str,
-        destf: str,
-        make_csv: str,
-        start_time: int,
-        end_time: int,
-        file_time: str
-) -> None:
+def write_cache_sqlite_to_kml(args: ConversionArgs) -> None:
 
-    # Get the time the program began to execute.
+    # Get the time the program began
     file_start_time = time.perf_counter()
 
     query_command_string = (
@@ -37,11 +29,11 @@ def write_cache_sqlite_to_kml(
     # Generate the SQL query to include the start_time and end_time values.
     CACHE_SQLITE_QUERY = cache_sqlite_query(
         start_time=start_time,
-        end_time=end_time
+        end_time=end_time,
     )
 
     # Query the database file.
-    df = hf.query_database(
+    df = Utils.query_database(
         source=source,
         query=CACHE_SQLITE_QUERY
     )
@@ -51,21 +43,21 @@ def write_cache_sqlite_to_kml(
 
     # Print verification message to screen.
     c.print(
-        f"\n[grey66][-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
+        f"\n[grey66]Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
         "of data\n"
     )
 
     # Set output file to the correct format.
-    output_kml_file = hf.get_destf_name(
+    output_kml_file = Utils.get_destf_name(
         dest=dest,
         destf=destf,
-        time=file_time
+        time=file_time,
     )
 
-    # Open the output file using the context manager.
+    # Open the output file using the context manager
     with open(output_kml_file, "w", encoding="utf-8") as f:
 
-        # Write the header data of the output .kml file.
+        # Write the header data of the output .kml file
         kml_header = cache_sqlite_kml_file_header()
 
         f.write(kml_header)
@@ -73,7 +65,7 @@ def write_cache_sqlite_to_kml(
         # Initialize a counter variable to keep track of number of records.
         count = 0
 
-        # Set variables from the dataframe.
+        # Set variables from the dataframe
         for index, row in df.iterrows():
             record = row["record_number"]
             Z_PK = row["z_pk"]
@@ -91,13 +83,13 @@ def write_cache_sqlite_to_kml(
             vert_acc_feet = row["vertical_accuracy_feet"]
             data_source = row["data_source"]
 
-            # Print message to screen with each record number added.
+            # Print message to screen with each record number added
             c.print(
                 f"    [grey66]Processing Row #: [dodger_blue1]{record:04d} "
                 "[grey66]| Z_PK #: [dodger_blue1]{Z_PK}"
             )
 
-            # Write the data from each record to the output .kml file.
+            # Write the data from each record to the .kml file
             kml_body = cache_sqlite_kml_file_body(
                 record=record,
                 local_time=local_time,
@@ -112,41 +104,38 @@ def write_cache_sqlite_to_kml(
                 horiz_acc_feet=horiz_acc_feet,
                 vert_acc_meters=vert_acc_meters,
                 vert_acc_feet=vert_acc_feet,
-                data_source=data_source
+                data_source=data_source,
             )
 
             f.write(kml_body)
 
-            # Increment the counter variable for the next record.
+            # Increment the counter variable for the next record
             count += 1
 
-        # Write the closing data to the output .kml file.
-        f.write(f"{hf.write_kml_closing()}")
+        # Write the closing data to the .kml file
+        f.write(f"{Utils.write_kml_closing()}")
 
-    # If the user chose to make a .csv file containing the parsed records.
+    # If the user chose to make a .csv file containing the parsed records
     if make_csv.lower() == "y":
-
-        output_csv_file = hf.get_csv_file_name(
+        output_csv_file = Utils.get_csv_file_name(
             dest=dest,
             destf=destf,
-            time=file_time
+            time=file_time,
         )
-
         df.to_csv(
             output_csv_file,
-            index=False
+            index=False,
         )
-
     else:
         pass
 
-    # Get the time the script completed.
+    # Get the time the script completed
     ending_time = time.perf_counter()
 
-    # Get the total time the script took to complete.
+    # Get the total time the script took to complete
     total_time = ending_time - file_start_time
 
-    hf.end_program(
+    Utils.end_program(
         query_command_string=query_command_string,
         number_of_rows=number_of_rows,
         start_time=start_time,
@@ -154,8 +143,8 @@ def write_cache_sqlite_to_kml(
         output_csv_file=output_csv_file,
         count=count,
         output_kml_file=output_kml_file,
-        total_time=total_time
+        total_time=total_time,
     )
 
-    # Ask user if they want to automatically open the output file.
-    hf.ask_open_output_kml_file(kml_file=output_kml_file)
+    # Ask user if they want to open the .kml file
+    Utils.ask_open_output_kml_file(kml_file=output_kml_file)

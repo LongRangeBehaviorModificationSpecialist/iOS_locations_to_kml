7842603337039140aacb679e72c6817edd37b375
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-08-10 20:25:39 -0400

    Update cache_encb_db_wifi_to_kml.py

--- cache_encb_db_wifi_to_kml.py
+++ cache_encb_db_wifi_to_kml.py
@@ -4,10 +4,11 @@ from rich.console import Console
 import time
 
 from functions.functions import HelperFunctions as hf
-from vars.cache_encb_wifi import(
+from vars.cache_encb_wifi import (
     cache_encb_db_wifi_query,
     cache_encb_db_wifi_kml_file_header,
-    cache_encb_db_wifi_kml_file_body)
+    cache_encb_db_wifi_kml_file_body
+)
 
 c = Console()
 
@@ -19,7 +20,8 @@ def write_cache_encb_db_wifi_to_kml(
         make_csv: str,
         start_time: int,
         end_time: int,
-        file_time: str) -> None:
+        file_time: str
+) -> None:
 
     # Get the time the program began to execute.
     file_start_time = time.perf_counter()
@@ -31,28 +33,33 @@ def write_cache_encb_db_wifi_to_kml(
     # Generate the SQL query to include the start_time and end_time values.
     CACHE_ENCRYPTEDB_WIFI_QUERY = cache_encb_db_wifi_query(
         start_time=start_time,
-        end_time=end_time)
+        end_time=end_time
+    )
 
     # Query the database file.
     df = hf.query_database(
         source=source,
-        query=CACHE_ENCRYPTEDB_WIFI_QUERY)
+        query=CACHE_ENCRYPTEDB_WIFI_QUERY
+    )
 
     # Get the total number of records in the worksheet.
     number_of_rows = len(df)
 
     # Print verification message to screen.
-    c.print(f"""\n[grey66]
-[-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows of data\n""")
+    c.print(
+        f"\n[grey66][-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
+        "of data\n"
+    )
 
     # Set output file to the correct format.
     output_kml_file = hf.get_destf_name(
         dest=dest,
         destf=destf,
-        time=file_time)
+        time=file_time
+    )
 
     # Open the output file using the context manager.
-    with open(f"{output_kml_file}", "w", encoding="utf-8") as f:
+    with open(output_kml_file, "w", encoding="utf-8") as f:
 
         # Write the header data of the output .kml file.
         kml_header = cache_encb_db_wifi_kml_file_header()
@@ -78,7 +85,9 @@ def write_cache_encb_db_wifi_to_kml(
             # Print message to screen with each record number added.
             # DO NOT add Z_PK for this query -- the database table does not
             # have a primary key.
-            c.print(f"\t[grey66]Processing Row #: [dodger_blue1]{record:04d}")
+            c.print(
+                f"    [grey66]Processing Row #: [dodger_blue1]{record:04d}"
+            )
 
             # Write the data from each record to the output .kml file.
             kml_body = cache_encb_db_wifi_kml_file_body(
@@ -91,7 +100,8 @@ def write_cache_encb_db_wifi_to_kml(
                 utc_time=utc_time,
                 altitude=altitude,
                 confidence=confidence,
-                data_source=data_source)
+                data_source=data_source
+            )
 
             f.write(kml_body)
 
@@ -107,11 +117,13 @@ def write_cache_encb_db_wifi_to_kml(
         output_csv_file = hf.get_csv_file_name(
             dest=dest,
             destf=destf,
-            time=file_time)
+            time=file_time
+        )
 
         df.to_csv(
             output_csv_file,
-            index=False)
+            index=False
+        )
 
     else:
         pass
@@ -130,7 +142,8 @@ def write_cache_encb_db_wifi_to_kml(
         output_csv_file=output_csv_file,
         count=count,
         output_kml_file=output_kml_file,
-        total_time=total_time)
+        total_time=total_time
+    )
 
     # Ask user if they want to automatically open the output file.
     hf.ask_open_output_kml_file(kml_file=output_kml_file)

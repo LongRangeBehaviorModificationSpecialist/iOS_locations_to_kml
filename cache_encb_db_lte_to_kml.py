65330513406e908fe3ffb04e1e6f1a7e4915fc82
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-08-11 17:21:01 -0400

    Update cache_encb_db_lte_to_kml.py

--- cache_encb_db_lte_to_kml.py
+++ cache_encb_db_lte_to_kml.py
@@ -118,7 +118,7 @@ python .\{python_file} --source "{source}" --dest "{dest}" --destf "{destf}" \
                 destf=destf,
                 time=file_time,
             )
-            df.to_csv(output_csv_file, index=False)
+            df.to_csv(csv_file, index=False)
         case "n":
             pass
 

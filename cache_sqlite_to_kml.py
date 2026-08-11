96e9a60a5c9120e8030e82f3fe5040524449031e
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-08-10 20:19:09 -0400

    Update cache_sqlite_to_kml.py

--- cache_sqlite_to_kml.py
+++ cache_sqlite_to_kml.py
@@ -4,10 +4,11 @@ from rich.console import Console
 import time
 
 from functions.functions import HelperFunctions as hf
-from vars.cache_sqlite import(
+from vars.cache_sqlite import (
     cache_sqlite_query,
     cache_sqlite_kml_file_header,
-    cache_sqlite_kml_file_body)
+    cache_sqlite_kml_file_body
+)
 
 
 c = Console()
@@ -21,40 +22,48 @@ def write_cache_sqlite_to_kml(
         make_csv: str,
         start_time: int,
         end_time: int,
-        file_time: str) -> None:
+        file_time: str
+) -> None:
 
     # Get the time the program began to execute.
     file_start_time = time.perf_counter()
 
-    query_command_string = f"""python .\{python_file} --source "{source}" \
---dest "{dest}" --destf "{destf}" --csv {make_csv} --db 1 --starttime \
-{start_time} --endtime {end_time}"""
+    query_command_string = (
+        f"python .\{python_file} --source '{source}' --dest '{dest}' --destf "
+        f"'{destf}' --csv '{make_csv}' --db 1 --starttime '{start_time}' --endtime "
+        f"'{end_time}'"
+    )
 
     # Generate the SQL query to include the start_time and end_time values.
     CACHE_SQLITE_QUERY = cache_sqlite_query(
         start_time=start_time,
-        end_time=end_time)
+        end_time=end_time
+    )
 
     # Query the database file.
     df = hf.query_database(
         source=source,
-        query=CACHE_SQLITE_QUERY)
+        query=CACHE_SQLITE_QUERY
+    )
 
     # Get the total number of records in the worksheet.
     number_of_rows = len(df)
 
     # Print verification message to screen.
-    c.print(f"""\n[grey66]
-[-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows of data\n""")
+    c.print(
+        f"\n[grey66][-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
+        "of data\n"
+    )
 
     # Set output file to the correct format.
     output_kml_file = hf.get_destf_name(
         dest=dest,
         destf=destf,
-        time=file_time)
+        time=file_time
+    )
 
     # Open the output file using the context manager.
-    with open(f"{output_kml_file}", "w", encoding="utf-8") as f:
+    with open(output_kml_file, "w", encoding="utf-8") as f:
 
         # Write the header data of the output .kml file.
         kml_header = cache_sqlite_kml_file_header()
@@ -83,8 +92,10 @@ def write_cache_sqlite_to_kml(
             data_source = row["data_source"]
 
             # Print message to screen with each record number added.
-            c.print(f"\t[grey66]Processing Row #: [dodger_blue1]{record:04d} \
-[grey66]| Z_PK #: [dodger_blue1]{Z_PK}")
+            c.print(
+                f"    [grey66]Processing Row #: [dodger_blue1]{record:04d} "
+                "[grey66]| Z_PK #: [dodger_blue1]{Z_PK}"
+            )
 
             # Write the data from each record to the output .kml file.
             kml_body = cache_sqlite_kml_file_body(
@@ -101,7 +112,8 @@ def write_cache_sqlite_to_kml(
                 horiz_acc_feet=horiz_acc_feet,
                 vert_acc_meters=vert_acc_meters,
                 vert_acc_feet=vert_acc_feet,
-                data_source=data_source)
+                data_source=data_source
+            )
 
             f.write(kml_body)
 
@@ -117,11 +129,13 @@ def write_cache_sqlite_to_kml(
         output_csv_file = hf.get_csv_file_name(
             dest=dest,
             destf=destf,
-            time=file_time)
+            time=file_time
+        )
 
         df.to_csv(
             output_csv_file,
-            index=False)
+            index=False
+        )
 
     else:
         pass
@@ -140,8 +154,8 @@ def write_cache_sqlite_to_kml(
         output_csv_file=output_csv_file,
         count=count,
         output_kml_file=output_kml_file,
-        total_time=total_time)
+        total_time=total_time
+    )
 
     # Ask user if they want to automatically open the output file.
     hf.ask_open_output_kml_file(kml_file=output_kml_file)
-

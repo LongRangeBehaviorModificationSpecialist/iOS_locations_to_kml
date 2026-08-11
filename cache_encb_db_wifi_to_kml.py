a724cd1981c94ee6b999723d2def8484af96366d
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-08-11 16:22:27 -0400

    Update cache_encb_db_wifi_to_kml.py

--- cache_encb_db_wifi_to_kml.py
+++ cache_encb_db_wifi_to_kml.py
@@ -3,7 +3,7 @@
 from rich.console import Console
 import time
 
-from functions.functions import HelperFunctions as hf
+from functions.functions import Utils
 from vars.cache_encb_wifi import (
     cache_encb_db_wifi_query,
     cache_encb_db_wifi_kml_file_header,
@@ -23,53 +23,53 @@ def write_cache_encb_db_wifi_to_kml(
         file_time: str
 ) -> None:
 
-    # Get the time the program began to execute.
+    # Time the program began
     file_start_time = time.perf_counter()
 
     query_command_string = f"""python .\{python_file} --source "{source}" \
 --dest "{dest}" --destf "{destf}" --csv {make_csv} --db 2 --starttime \
 {start_time} --endtime {end_time}"""
 
-    # Generate the SQL query to include the start_time and end_time values.
+    # Generate the SQL query
     CACHE_ENCRYPTEDB_WIFI_QUERY = cache_encb_db_wifi_query(
         start_time=start_time,
         end_time=end_time
     )
 
-    # Query the database file.
-    df = hf.query_database(
+    # Query the database
+    df = Utils.query_database(
         source=source,
         query=CACHE_ENCRYPTEDB_WIFI_QUERY
     )
 
-    # Get the total number of records in the worksheet.
+    # Get the total number of records in the worksheet
     number_of_rows = len(df)
 
-    # Print verification message to screen.
+    # Print verification message to screen
     c.print(
-        f"\n[grey66][-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
+        f"\n[grey66]Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
         "of data\n"
     )
 
-    # Set output file to the correct format.
-    output_kml_file = hf.get_destf_name(
+    # Set .kml file name
+    kml_file = Utils.get_destf_name(
         dest=dest,
         destf=destf,
-        time=file_time
+        time=file_time,
     )
 
-    # Open the output file using the context manager.
-    with open(output_kml_file, "w", encoding="utf-8") as f:
+    # Open the output file
+    with open(kml_file, "w", encoding="utf-8") as f:
 
-        # Write the header data of the output .kml file.
+        # Write the header of the .kml file
         kml_header = cache_encb_db_wifi_kml_file_header()
 
         f.write(kml_header)
 
-        # Initialize a counter variable to keep track of number of records.
+        # Initialize a counter variable
         count = 0
 
-        # Set variables from the dataframe.
+        # Set variables from the dataframe
         for index, row in df.iterrows():
             record = row["record_number"]
             mac_address = row["mac_address"]
@@ -89,7 +89,7 @@ def write_cache_encb_db_wifi_to_kml(
                 f"    [grey66]Processing Row #: [dodger_blue1]{record:04d}"
             )
 
-            # Write the data from each record to the output .kml file.
+            # Write data from each record to the .kml file
             kml_body = cache_encb_db_wifi_kml_file_body(
                 record=record,
                 latitude=latitude,
@@ -100,51 +100,46 @@ def write_cache_encb_db_wifi_to_kml(
                 utc_time=utc_time,
                 altitude=altitude,
                 confidence=confidence,
-                data_source=data_source
+                data_source=data_source,
             )
 
             f.write(kml_body)
 
-            # Increment the counter variable for the next record.
+            # Increment the counter variable for the next record
             count += 1
 
-        # Write the closing data to the output .kml file.
-        f.write(f"{hf.write_kml_closing()}")
+        # Write closing data to the .kml file
+        f.write(f"{Utils.write_kml_closing()}")
 
-    # If the user chose to make a .csv file containing the parsed records.
-    if make_csv.lower() == "y":
+    # If the user chose to make a .csv file
+    match make_csv:
+        case "y":
+            csv_file = Utils.get_csv_file_name(
+                dest=dest,
+                destf=destf,
+                time=file_time,
+            )
+            df.to_csv(csv_file, index=False)
+        case "n":
+            pass
 
-        output_csv_file = hf.get_csv_file_name(
-            dest=dest,
-            destf=destf,
-            time=file_time
-        )
-
-        df.to_csv(
-            output_csv_file,
-            index=False
-        )
-
-    else:
-        pass
-
-    # Get the time the script completed.
+    # Time the script completed
     ending_time = time.perf_counter()
 
-    # Get the total time the script took to complete.
+    # Total time the script took to complete
     total_time = ending_time - file_start_time
 
-    hf.end_program(
+    Utils.end_program(
         query_command_string=query_command_string,
         number_of_rows=number_of_rows,
         start_time=start_time,
         end_time=end_time,
-        output_csv_file=output_csv_file,
+        output_csv_file=csv_file,
         count=count,
-        output_kml_file=output_kml_file,
-        total_time=total_time
+        output_kml_file=kml_file,
+        total_time=total_time,
     )
 
-    # Ask user if they want to automatically open the output file.
-    hf.ask_open_output_kml_file(kml_file=output_kml_file)
+    # Ask user if they want to open the output file
+    Utils.ask_open_output_kml_file(kml_file=kml_file)
 

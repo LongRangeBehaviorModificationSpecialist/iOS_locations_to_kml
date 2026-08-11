67d65eaf53b96c5f2c9d3623d4b8b65c5c452e6a
[@mikespon] <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2026-08-10 20:29:02 -0400

    Update cloud_v2_sqlite_signif_loc_to_kml.py

--- cloud_v2_sqlite_signif_loc_to_kml.py
+++ cloud_v2_sqlite_signif_loc_to_kml.py
@@ -4,10 +4,11 @@ from rich.console import Console
 import time
 
 from functions.functions import HelperFunctions as hf
-from vars.cloud_v2_sqlite_signif_loc import(
+from vars.cloud_v2_sqlite_signif_loc import (
     cloud_v2_sqlite_signif_loc_query,
     cloud_v2_sqlite_signif_loc_kml_file_header,
-    cloud_v2_sqlite_signif_loc_kml_file_body)
+    cloud_v2_sqlite_signif_loc_kml_file_body
+)
 
 c = Console()
 
@@ -19,7 +20,8 @@ def write_cache_v2_signif_loc_to_kml(
         make_csv: str,
         start_time: int,
         end_time: int,
-        file_time: str) -> None:
+        file_time: str
+) -> None:
 
     # Get the time the program began to execute.
     file_start_time = time.perf_counter()
@@ -31,28 +33,33 @@ def write_cache_v2_signif_loc_to_kml(
     # Generate the SQL query to include the start_time and end_time values.
     CLOUDV2_SIG_LOC_QUERY = cloud_v2_sqlite_signif_loc_query(
         start_time=start_time,
-        end_time=end_time)
+        end_time=end_time
+    )
 
     # Query the database file.
     df = hf.query_database(
         source=source,
-        query=CLOUDV2_SIG_LOC_QUERY)
+        query=CLOUDV2_SIG_LOC_QUERY
+    )
 
     # Get the total number of records in the worksheet.
     number_of_rows = len(df)
 
     # Print verification message to screen.
-    c.print(f"""\n[grey66]
-[-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows of data\n""")
+    c.print(
+        f"\n[grey66][-] Found [dodger_blue1]{number_of_rows:,} [grey66]rows "
+        "of data\n"
+    )
 
     # Set output file to the correct format.
     output_kml_file = hf.get_destf_name(
         dest=dest,
         destf=destf,
-        time=file_time)
+        time=file_time
+    )
 
     # Open the output file using the context manager.
-    with open(f"{output_kml_file}", "w", encoding="utf-8") as f:
+    with open(output_kml_file, "w", encoding="utf-8") as f:
 
         # Write the header data of the output .kml file.
         kml_header = cloud_v2_sqlite_signif_loc_kml_file_header()
@@ -76,8 +83,10 @@ def write_cache_v2_signif_loc_to_kml(
             data_source = row["data_source"]
 
             # Print message to screen with each record number added.
-            c.print(f"\t[grey66]Processing Row #: [dodger_blue1]{record:04d} \
-[grey66]| Z_PK #: [dodger_blue1]{Z_PK}")
+            c.print(
+                f"    [grey66]Processing Row #: [dodger_blue1]{record:04d} "
+                f"[grey66]| Z_PK #: [dodger_blue1]{Z_PK}"
+            )
 
             # Write the data from each record to the output .kml file.
             kml_body = cloud_v2_sqlite_signif_loc_kml_file_body(
@@ -90,7 +99,8 @@ def write_cache_v2_signif_loc_to_kml(
                 uncertainty=uncertainty,
                 add_create_utc=add_create_utc,
                 add_expire_utc=add_expire_utc,
-                data_source=data_source)
+                data_source=data_source
+            )
 
             f.write(kml_body)
 
@@ -106,11 +116,13 @@ def write_cache_v2_signif_loc_to_kml(
         output_csv_file = hf.get_csv_file_name(
             dest=dest,
             destf=destf,
-            time=file_time)
+            time=file_time
+        )
 
         df.to_csv(
             output_csv_file,
-            index=False)
+            index=False
+        )
 
     else:
         pass
@@ -129,8 +141,8 @@ def write_cache_v2_signif_loc_to_kml(
         output_csv_file=output_csv_file,
         count=count,
         output_kml_file=output_kml_file,
-        total_time=total_time)
+        total_time=total_time
+    )
 
     # Ask user if they want to automatically open the output file.
     hf.ask_open_output_kml_file(kml_file=output_kml_file)
-
